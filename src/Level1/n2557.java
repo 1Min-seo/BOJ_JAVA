349f7505a181203913043a2ec037082191503dac
1Min-seo <readex11@naver.com>
2023-01-08 00:02:26 +0900

    create 2557.java

--- src/Level1/n2557.java
+++ src/Level1/n2557.java
@@ -1,10 +1,36 @@
 package Level1;
+import java.util.*;
+import java.io.*;
 
 public class n2557 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        FastReader fr = new FastReader(); // 문제 해결 소스 코드
+        
+        System.out.println("Hello World!");
+    }
 
-	}
 
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
 }
