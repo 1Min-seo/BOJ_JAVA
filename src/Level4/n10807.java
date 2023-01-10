579cbde7c5b2db55141eb21757de3b1488e90c3d
1Min-seo <readex11@naver.com>
2023-01-10 16:18:13 +0900

    create 10807.java

---
+++ src/Level4/n10807.java
@@ -0,0 +1,52 @@
+package Level4;
+import java.util.*;
+import java.io.*;
+
+public class n10807 {
+
+	public static void main(String[] args) {
+        FastReader fr = new FastReader(); // 문제 해결 소스 코드
+        int N=fr.nextInt();
+        int []arr=new int[N];
+        int cnt=0;
+         
+        for(int i=0; i<N; i++) {
+        	arr[i]=fr.nextInt();
+        }
+        
+        int v=fr.nextInt();
+        
+        for(int i=0; i<N; i++) {
+        	if(arr[i]==v) {
+        		cnt+=1;
+        	}
+        }
+        
+        System.out.println(cnt);
+    }
+
+
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
+
