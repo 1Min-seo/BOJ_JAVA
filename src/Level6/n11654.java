c683fc7157ede275969972a35986595f83c9689d
1Min-seo <readex11@naver.com>
2023-01-12 01:29:46 +0900

    create 11654.java

---
+++ src/Level6/n11654.java
@@ -0,0 +1,39 @@
+package Level6;
+import java.util.*;
+import java.io.*;
+
+public class n11654 {
+
+	public static void main(String[] args) {
+        FastReader fr = new FastReader(); // 문제 해결 소스 코드
+        
+        int ch=fr.next().charAt(0);
+        System.out.println(ch);
+        
+        
+    }
+
+
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
