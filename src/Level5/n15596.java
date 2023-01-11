a67bec539b0710d0aa8098e02b2299f2ce2292e8
1Min-seo <readex11@naver.com>
2023-01-11 18:57:42 +0900

    create 15596.java

---
+++ src/Level5/n15596.java
@@ -0,0 +1,17 @@
+package Level5;
+import java.util.*;
+import java.io.*;
+
+public class n15596 {
+	
+	long sum(int[] a) {
+		long sum=0;
+		
+		for(int i=0; i<a.length; i++) {
+			sum+=a[i];
+		}
+		return sum;
+	}
+}
+
+
