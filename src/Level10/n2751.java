c17247195b86708bd1269e95a221bad783abd915
1Min-seo <readex11@naver.com>
2023-01-14 22:05:08 +0900

    create 2571.java

---
+++ src/Level10/n2751.java
@@ -0,0 +1,29 @@
+package Level10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class n2751 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb=new StringBuilder();
+		ArrayList<Integer> arr=new ArrayList<Integer>();
+		
+		int N=Integer.parseInt(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr.add(Integer.parseInt(br.readLine()));
+		}
+		
+		Collections.sort(arr);
+		
+		for(Integer value:arr) {
+			sb.append(value).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
