ee9b80040d432c1eb5497056c7de3b404596ece5
1Min-seo <readex11@naver.com>
2023-01-18 16:01:55 +0900

    create 1541.java

--- src/Level19/n1541.java
+++ src/Level19/n1541.java
@@ -6,33 +6,35 @@ import java.io.InputStreamReader;
 public class n1541 {
 
 	public static void main(String[] args)throws Exception {
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		String data=br.readLine();
 		int result=0;
 		
 		int minusIndex=data.indexOf("-");
 		if(minusIndex==-1) {
-			String[] arr=data.split("\\+");
+			String []arr=data.split("\\+");
 			for(String value:arr) {
 				result+=Integer.parseInt(value);
 			}
-		}else {
-			String add=data.substring(0,minusIndex);
-			String minus=data.substring(minusIndex+1);
+		}
+		else {
+			String front=data.substring(0, minusIndex);
+			String back=data.substring(minusIndex+1);
 			
-			String[]arr=add.split("\\+");
-			for(String valueP:arr) {
-				result+=Integer.parseInt(valueP);
+			String[] frontArr=front.split("\\+");
+			for(String frontValue:frontArr) {
+				result+=Integer.parseInt(frontValue);
 			}
 			
-			arr=minus.split("-");
-			for(String valueM:arr) {
-				String []subArr=valueM.split("\\+");
-				for(String valueMM:subArr) {
-					result-=Integer.parseInt(valueMM);
+			String[] backArr=back.split("-");
+			for(String backMinusValue:backArr) {
+				String []backPlusArr=backMinusValue.split("\\+");
+				for(String backPlusValue: backPlusArr) {
+					result-=Integer.parseInt(backPlusValue);
 				}
 			}
 		}
+		
 		System.out.println(result);
 	}
 }
