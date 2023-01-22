30bfc162c91fd38ac1fd707930ae0c3dbd7d3934
1Min-seo <readex11@naver.com>
2023-01-22 20:51:10 +0900

    create 1920.java

--- src/Level23/n1920.java
+++ src/Level23/n1920.java
@@ -1,50 +1,51 @@
 package Level23;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class n1920 {
-	
-	public static int binarySearch(int []arr, int target, int start, int end) {
-		while(start<=end) {
+	public static int lowerBound(int []arr, int target,int start, int end) {
+		while(start<end) {
 			int mid=(start+end)/2;
-			if(arr[mid]==target) return 1;
-			else if(arr[mid]>target) {
-				end=mid-1;
-			}
-			else if(arr[mid]<target) {
-				start=mid+1;
-			}
+			if(target<=arr[mid]) end=mid;
+			else start=mid+1;
 		}
-		return 0;
+		return end;
 	}
-
+	
+	
+	public static int upperBound(int []arr, int target, int start, int end) {
+		while(start<end){
+			int mid=(start+end)/2;
+			if(target<arr[mid]) end=mid;
+			else start=mid+1;
+		}
+		return end;
+	}
 	public static void main(String[] args)throws Exception {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N=Integer.parseInt(br.readLine());
-		int []arr1=new int[N];
-		StringTokenizer st=new StringTokenizer(br.readLine());
+		int []my=new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
-			arr1[i]=Integer.parseInt(st.nextToken());
+			my[i]=Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(arr1);
-		
+		Arrays.sort(my);
+		 
 		int M=Integer.parseInt(br.readLine());
-		int []arr2=new int[M];
-		st=new StringTokenizer(br.readLine());
-		for(int i=0; i<M; i++) {
-			arr2[i]=Integer.parseInt(st.nextToken());
-		}
+		int []arr=new int[M];
 		
+		st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<M; i++) {
-			int result=-1;
-			result= binarySearch(arr1,arr2[i] ,0,N-1);
-			System.out.println(result);
+			int x=Integer.parseInt(st.nextToken());
+			sb.append(upperBound(my,x,0,my.length)-lowerBound(my,x,0,my.length)).append(' ');
 		}
-		
+		System.out.println(sb);
 	}
 
 }
