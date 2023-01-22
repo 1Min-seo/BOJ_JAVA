ab16664bdbc638844a4c43c020a1b5b0648c9edf
1Min-seo <readex11@naver.com>
2023-01-22 20:59:52 +0900

    create 10816.java

--- src/Level23/n10816.java
+++ src/Level23/n10816.java
@@ -6,53 +6,45 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class n10816 {
-	public static int lowerBound(int []arr, int target, int start, int end) {
+	public static int lowerBound(int []arr, int target,int start, int end) {
 		while(start<end) {
 			int mid=(start+end)/2;
-			if(arr[mid]>=target) end=mid+1;
+			if(target<=arr[mid]) end=mid;
 			else start=mid+1;
 		}
 		return end;
 	}
+	
 	
 	public static int upperBound(int []arr, int target, int start, int end) {
-		while(start<end) {
+		while(start<end){
 			int mid=(start+end)/2;
-			if(arr[mid]>target) end=mid+1;
+			if(target<arr[mid]) end=mid;
 			else start=mid+1;
 		}
 		return end;
 	}
-	
-	public static int countByRange(int []arr, int leftValue, int rightValue) {
-		int rightIndex=upperBound(arr, rightValue, 0, arr.length);
-		int leftIndex=lowerBound(arr, leftValue, 0, arr.length);
-		return rightIndex-leftIndex;
-	}
 	public static void main(String[] args)throws Exception {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N=Integer.parseInt(br.readLine());
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		int []my=new int[N]; //상근이가 가지고 있는 카드
+		int []my=new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
 			my[i]=Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(my);
-		
+		 
 		int M=Integer.parseInt(br.readLine());
-		int []data=new int[M]; //보기 카드
-		st=new StringTokenizer(br.readLine());
-		for(int i=0; i<M; i++) {
-			data[i]=Integer.parseInt(st.nextToken());
-		}
+		int []arr=new int[M];
 		
+		st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<M; i++) {
-			int result=0;
-			result=countByRange(my,data[i],data[i]);
-			System.out.print(result+" ");
+			int x=Integer.parseInt(st.nextToken());
+			sb.append(upperBound(my,x,0,my.length)-lowerBound(my,x,0,my.length)).append(' ');
 		}
-		
+		System.out.println(sb);
 	}
 
 }
