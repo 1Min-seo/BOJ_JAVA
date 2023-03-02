663ee7a4906de821990bd769cc71aab24ee7706f
1Min-seo <readex11@naver.com>
2023-03-02 23:07:14 +0900

    create 10816.java

--- src/Level23/n10816.java
+++ src/Level23/n10816.java
@@ -6,23 +6,38 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class n10816 {
-	public static int lowerBound(int []arr, int target,int start, int end) {
+	public static int lowerBound(int []arr, int target, int start, int end) {
 		while(start<end) {
 			int mid=(start+end)/2;
-			if(target<=arr[mid]) end=mid;
+			
+			if(target<=arr[mid]) {
+				end=mid;
+			}
 			else start=mid+1;
 		}
 		return end;
 	}
 	
 	public static int upperBound(int []arr, int target, int start, int end) {
-		while(start<end){
+		while(start<end) {
 			int mid=(start+end)/2;
-			if(target<arr[mid]) end=mid;
+			
+			if(target<arr[mid]) {
+				end=mid;
+			}
 			else start=mid+1;
 		}
+		
 		return end;
+		
 	}
+	
+	public static int getLength(int lowerBound, int upperBound) {
+		int result=upperBound-lowerBound;
+		
+		return result;
+	}
+	
 	public static void main(String[] args)throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N=Integer.parseInt(br.readLine());
@@ -32,17 +47,29 @@ public class n10816 {
 		for(int i=0; i<N; i++) {
 			my[i]=Integer.parseInt(st.nextToken());
 		}
+		
 		Arrays.sort(my);
-		 
+		
+		
 		int M=Integer.parseInt(br.readLine());
 		int []arr=new int[M];
 		
 		st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<M; i++) {
-			int x=Integer.parseInt(st.nextToken());
-			sb.append(upperBound(my,x,0,my.length)-lowerBound(my,x,0,my.length)).append(' ');
+			arr[i]=Integer.parseInt(st.nextToken());
 		}
+		
+		StringBuilder sb=new StringBuilder();
+		
+		for(int i=0; i<M; i++) {
+			int low= lowerBound(my,arr[i],0,my.length);
+			int hi=upperBound(my,arr[i],0,my.length);
+			int result=getLength(low,hi);
+			
+			sb.append(result).append(' ');
+		}
+		
 		System.out.println(sb);
 	}
+
 }
