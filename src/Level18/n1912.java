51ec304b59009397571bb7257a93575e031c89d0
1Min-seo <readex11@naver.com>
2023-03-18 15:36:28 +0900

    create 1912.java

---
+++ src/Level18/n1912.java
@@ -0,0 +1,50 @@
+package Level18;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class n1912 {
+
+	public static void main(String[] args)throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int n=Integer.parseInt(br.readLine());
+		int []arr=new int[n];
+		int []dp=new int[n];
+		
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		dp[0]=arr[0];
+		int result=-1001;
+		
+		for(int i=1; i<n; i++) {
+			dp[i]=Math.max(dp[i-1]+arr[i], arr[i]);
+		}
+		
+		for(int i=0; i<n; i++) {
+			result=Math.max(result, dp[i]);
+		}
+		
+		System.out.println(result);
+	}
+}
+/*
+ * 10	-4	3	1	5	6	-35	12	21	-1
+ * 연속된 몇 개의 수를 선택해서 구할 수 있는 합 중 가장 큰 합 구하기
+ * 12+21 = > 33
+ * 
+ * dp 배열의 값
+ * 
+ * max(10 + -4 , -4) -> 6
+ * max(6+3,3) -> 9
+ * max(9+1,1) -> 10
+ * 
+ * 
+ * dp 중에서 제일 큰 값 구하기
+ * 이므로 정답은 33이다.
+ * 
+*/
