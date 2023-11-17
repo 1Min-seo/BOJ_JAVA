1441fa7c986a022a77252b563950b5a592cf0801
Minseo Han <readex11@naver.com>
2023-11-17 16:17:32 +0900

    create 10816.java

--- src/Level27/n10816.java
+++ src/Level27/n10816.java
@@ -6,44 +6,12 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class n10816 {
-	public static int lowerBound(int []arr, int target, int start, int end) {
-		while(start<end) {
-			int mid=(start+end)/2;
-			
-			if(target<=arr[mid]) {
-				end=mid;
-			}
-			else start=mid+1;
-		}
-		return end;
-	}
-	
-	public static int upperBound(int []arr, int target, int start, int end) {
-		while(start<end) {
-			int mid=(start+end)/2;
-			
-			if(target<arr[mid]) {
-				end=mid;
-			}
-			else start=mid+1;
-		}
-		
-		return end;
-		
-	}
-	
-	public static int getLength(int lowerBound, int upperBound) {
-		int result=upperBound-lowerBound;
-		
-		return result;
-	}
-	
 	public static void main(String[] args)throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int N=Integer.parseInt(br.readLine());
 		int []my=new int[N];
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st=new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
 			my[i]=Integer.parseInt(st.nextToken());
 		}
@@ -51,24 +19,51 @@ public class n10816 {
 		Arrays.sort(my);
 		
 		int M=Integer.parseInt(br.readLine());
-		int []arr=new int[M];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<M; i++) {
-			arr[i]=Integer.parseInt(st.nextToken());
-		}
-		
+		st=new StringTokenizer(br.readLine());
 		StringBuilder sb=new StringBuilder();
 		
 		for(int i=0; i<M; i++) {
-			int low= lowerBound(my,arr[i],0,my.length);
-			int hi=upperBound(my,arr[i],0,my.length);
-			int result=getLength(low,hi);
+			int key=Integer.parseInt(st.nextToken());
 			
-			sb.append(result).append(' ');
+			sb.append(upperBound(my,key)-lowerBound(my,key)).append(' ');
+		}
+		System.out.println(sb);
+		
+	}
+	
+	public static int lowerBound(int[] my, int key) {
+		int lo=0;
+		int hi=my.length;
+		
+		while(lo<hi) {
+			int mid=(lo+hi)/2;
+			
+			if(key<=my[mid]) {
+				hi=mid;
+			}
+			else {
+				lo=mid+1;
+			}
 		}
 		
-		System.out.println(sb);
+		return lo;
+	}
+	public static int upperBound(int[] my, int key) {
+		int lo=0;
+		int hi=my.length;
+		
+		while(lo<hi){
+			int mid=(lo+hi)/2;
+			
+			if(key<my[mid]) {
+				hi=mid;
+			}
+			else {
+				lo=mid+1;
+			}
+		}
+		
+		return lo;
 	}
 
 }
