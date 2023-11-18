66e95b616103ea07eb45cf34bf11f9977151d4d4
Minseo Han <readex11@naver.com>
2023-11-18 16:15:15 +0900

    create 10816.java

--- src/Level27/n10816.java
+++ src/Level27/n10816.java
@@ -6,6 +6,34 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class n10816 {
+	public static int lowerBound(int []my, int target) {
+		int lo=0;
+		int hi=my.length;
+		while(lo<hi) {
+			int mid=(lo+hi)/2;
+			if(target<=my[mid]) {
+				hi=mid;
+			}else {
+				lo=mid+1;
+			}
+		}
+		return lo;
+	}
+	
+	public static int upperBound(int []my, int target) {
+		int lo=0;
+		int hi=my.length;
+		while(lo<hi) {
+			int mid=(lo+hi)/2;
+			if(target<my[mid]) {
+				hi=mid;
+			}else {
+				lo=mid+1;
+			}
+		}
+		return lo;
+	}
+	
 	public static void main(String[] args)throws Exception {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int N=Integer.parseInt(br.readLine());
@@ -20,50 +48,20 @@ public class n10816 {
 		
 		int M=Integer.parseInt(br.readLine());
 		st=new StringTokenizer(br.readLine());
+		int []arr=new int[M];
+			
+		for(int i=0; i<M; i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		
 		StringBuilder sb=new StringBuilder();
 		
 		for(int i=0; i<M; i++) {
-			int key=Integer.parseInt(st.nextToken());
-			
-			sb.append(upperBound(my,key)-lowerBound(my,key)).append(' ');
+			sb.append(upperBound(my,arr[i])-lowerBound(my,arr[i])).append(' ');
 		}
-		System.out.println(sb);
 		
+		System.out.println(sb);
 	}
 	
-	public static int lowerBound(int[] my, int key) {
-		int lo=0;
-		int hi=my.length;
-		
-		while(lo<hi) {
-			int mid=(lo+hi)/2;
-			
-			if(key<=my[mid]) {
-				hi=mid;
-			}
-			else {
-				lo=mid+1;
-			}
-		}
-		
-		return lo;
-	}
-	public static int upperBound(int[] my, int key) {
-		int lo=0;
-		int hi=my.length;
-		
-		while(lo<hi){
-			int mid=(lo+hi)/2;
-			
-			if(key<my[mid]) {
-				hi=mid;
-			}
-			else {
-				lo=mid+1;
-			}
-		}
-		
-		return lo;
-	}
 
 }
