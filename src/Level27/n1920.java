01751bea66acefbaebb19322c21b6bec0793bf6d
Minseo Han <readex11@naver.com>
2023-11-18 14:15:35 +0900

    create 1920.java

--- src/Level27/n1920.java
+++ src/Level27/n1920.java
@@ -7,44 +7,46 @@ import java.util.StringTokenizer;
 
 public class n1920 {
 	
-	public static int binarySearch(int []arr, int target, int start, int end) {
-		while(start<=end) {
-			int mid=(start+end)/2;
-			if(arr[mid]==target) return 1;
-			else if(arr[mid]>target) {
-				end=mid-1;
-			}
-			else if(arr[mid]<target) {
-				start=mid+1;
+	public static int binarySearch(int []my,int key) {
+		int lo=0;
+		int hi=my.length-1;
+		
+		while(lo<=hi) {
+			int mid=(lo+hi)/2;
+			
+			if(my[mid]==key) {
+				return 1;
+			}else if(my[mid]>key) {
+				hi=mid-1;
+			}else if(my[mid]<key){
+				lo=mid+1;
 			}
 		}
 		return 0;
 	}
-
 	public static void main(String[] args)throws Exception {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		
 		int N=Integer.parseInt(br.readLine());
-		int []arr1=new int[N];
+		int []my=new int[N];
+		
 		StringTokenizer st=new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
-			arr1[i]=Integer.parseInt(st.nextToken());
+			my[i]=Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(arr1);
+		
+		Arrays.sort(my);
 		
 		int M=Integer.parseInt(br.readLine());
-		int []arr2=new int[M];
+		int []arr=new int[M];
+		
 		st=new StringTokenizer(br.readLine());
 		for(int i=0; i<M; i++) {
-			arr2[i]=Integer.parseInt(st.nextToken());
+			arr[i]=Integer.parseInt(st.nextToken());
 		}
 		
 		for(int i=0; i<M; i++) {
-			int result=-1;
-			result= binarySearch(arr1,arr2[i] ,0,N-1);
-			System.out.println(result);
+			System.out.println(binarySearch(my,arr[i]));
 		}
-		
 	}
-
 }
