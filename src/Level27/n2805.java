739ce60090fa060b363920935c237160c3f1e1c7
Minseo Han <readex11@naver.com>
2023-11-19 18:19:13 +0900

    create 2805.java

--- src/Level27/n2805.java
+++ src/Level27/n2805.java
@@ -2,36 +2,28 @@ package Level27;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class n2805 {
-	
-	public static int BinarySearch(int []trees, int start, int end, int target) {	
+	public static int binarySearch(int[]trees,int start, int end, int target) {
 		while(start<end) {
+			long total=0;
 			int mid=(start+end)/2;
-			int sum=0;
-			int remain=0;
-			for(int tree:trees) {
-				if(tree-mid<=0){
-					remain=0;
-				}else {
-					remain=tree-mid;
+			for(int i=0; i<trees.length; i++) {
+				if(trees[i]-mid>0) {
+					total+=(trees[i]-mid);
 				}
-				sum+=remain;
 			}
-			if(sum<target) {
+			if(total<target) {
 				end=mid;
-			}
-			else {
+			}else {
 				start=mid+1;
 			}
 		}
 		return start-1;
 	}
-	
+		
 	public static void main(String[] args)throws Exception {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st=new StringTokenizer(br.readLine());
@@ -47,27 +39,37 @@ public class n2805 {
 		
 		Arrays.sort(trees);
 		
-		int result=BinarySearch(trees, trees[0], trees[N-1],M);
-		
+		int result=binarySearch(trees,1, trees[trees.length-1], M);
 		System.out.println(result);
 	}
 }
 
 
 /*
- * n 나무 수  4 
- * m 집으로 가져가려고 하는 나무 길이  7
- * 높이의 최댓값 구하기
+ * total이 7이 되어야 함
+ * 10	15	17	20
  * 
- * 20 15 10 17
+ * lo=0	hi=21	mid=10
  * 
- * 10  15  17  20
+ * 0	5	2	10	total=17
  * 
- * 절단기 (10+20)/2 => 15인 경우
- * 5  0  0  2   => 7
- * target이 7이랑 같으니 절단기 길이 반환
+ * lo=11	hi=21	mid=16
  * 
+ * 0	0	1	4	total=5
  * 
- * 절단기가 나무 길이보다 크거나 같으면 0
- * 절단기가 나무 길이보다 작으면 뺀 값 더하기
+ * lo=16	hi=21	mid=18
+ * 
+ * 0	0	0	2	total=2
+ * 
+ * lo=16	hi=18	mid=17
+ * 
+ * 0	0	0	3	total=3
+ * 
+ * lo=16	hi=17	mid=16
+ * 
+ * 0	0	1	4	total=5
+ * 
+ * 10=16	hi=16	mid=16
+ * 
+ * return 16-1=15
  */
