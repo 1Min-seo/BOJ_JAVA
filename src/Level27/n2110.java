3f1ce7bec78f958d3c2556e83ee59c85e1ef8644
Minseo Han <readex11@naver.com>
2023-11-19 23:42:24 +0900

    create 2110.java

--- src/Level27/n2110.java
+++ src/Level27/n2110.java
@@ -6,25 +6,31 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class n2110 {
-	
-	public static int binarySearch(int []arr, int start, int end, int cnt) {
-		while(start<end) {
-			int result=1;
-			int mid=(start+end)/2;
-			int before=arr[0];	
-			for(int i=1; i<arr.length; i++) {
-				 if(arr[i]-before>=mid){
-					result+=1;
-					before=arr[i];
-				}
-			}
-			if(result<cnt) {
-				end=mid;
-			}
-			else start=mid+1;	
-		}
+	public static int binarySearch(int []arr, int start, int end, int target) {
 		
-		return end-1;
+		while(start<end) {
+			int mid=(start+end)/2;
+			
+			if(install(mid,arr)<target) {
+				end=mid;
+			}else{
+				start=mid+1;
+			}
+		}
+		return start-1;
+	}
+	
+	public static int install(int mid, int []arr) {
+		int cnt=1;
+		int lastLocate=arr[0];
+		
+		for(int i=1; i<arr.length; i++) {
+			if(arr[i]-lastLocate>=mid) {
+				cnt+=1;
+				lastLocate=arr[i];
+			}
+		}
+		return cnt;
 	}
 
 	public static void main(String[] args)throws Exception {
@@ -44,43 +50,6 @@ public class n2110 {
 		
 		System.out.println(result);
 		
-		
 	}
 
 }
-
-/*
-1   2   8   4   9
-
-1   2   4   8   9
-*		*	*
-*		*		*
-
-가장 거리가 짧은 경우  1
-가장 거리가 긴   경우  8
-
-최소 거리 4인 경우
-
-첫 번째 집	  1
-2-1  안됨
-4-1  안됨
-8-1  됨
-9-8  안됨
-
-
-가장 거리가 짧은 경우  1
-가장 거리가 긴   경우  3
-최소 거리가 2
-2-1  안됨
-4-1  됨
-8-4  됨
-
-
-1) 첫 번째 집은 무조건 카운트를 한다
-2) 비교했을 때 최소 거리보다 커야한다.
-3) 최소 거리보다 큰 경우 또 한번 카운트
-4) 직전 집을 다시 초기화
-5) 카운트가 부족한 경우 최소 거리 줄인다.
-6) 카운트가 C보다 큰 경우 최소 거리를 늘린다.
-
-*/
