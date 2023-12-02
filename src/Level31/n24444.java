1103ef5415962df001869803969dff8277fe9c67
Minseo Han <readex11@naver.com>
2023-12-02 15:19:46 +0900

    create 24444.java

--- src/Level31/n24444.java
+++ src/Level31/n24444.java
@@ -9,64 +9,63 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class n24444 {
-	static ArrayList<ArrayList<Integer>> graph=new ArrayList<>();
-	static boolean visited[];
-	static int vertex;
-	static int edge;
-	static int start;
+	static int node ,edge, start;
+	static int x,y;
+	static ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+	static int visited[];
+	static int cnt=1;
+	
 	public static void main(String[] args)throws Exception {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st=new StringTokenizer(br.readLine());
 		
-		vertex=Integer.parseInt(st.nextToken());
+		node=Integer.parseInt(st.nextToken());
 		edge=Integer.parseInt(st.nextToken());
 		start=Integer.parseInt(st.nextToken());
 		
-		visited=new boolean[vertex+1];
-		for(int i=0; i<=vertex; i++) {
-			graph.add(new ArrayList<Integer>());
+		for(int i=0; i<node+1; i++) {
+			arr.add(new ArrayList<Integer>());
 		}
 		
 		for(int i=0; i<edge; i++) {
 			st=new StringTokenizer(br.readLine());
-			int a=Integer.parseInt(st.nextToken());
-			int b=Integer.parseInt(st.nextToken());
+			x=Integer.parseInt(st.nextToken());
+			y=Integer.parseInt(st.nextToken());
 			
-			graph.get(a).add(b);
-			graph.get(b).add(a);
+			arr.get(x).add(y);
+			arr.get(y).add(x);
 		}
 		
-		for(int i=0; i<=vertex; i++) {
-			Collections.sort(graph.get(i));
+		for(int i=1; i<=node; i++) {
+			Collections.sort(arr.get(i));
 		}
+		
+		visited=new int[node+1];
 		
 		bfs(start);
-
+		for(int i=1; i<visited.length; i++) {
+			System.out.println(visited[i]);
+		}
 	}
 	
-	public static void bfs(int v) {
-		Queue<Integer>queue=new LinkedList<>();
-		int cnt=0;
-		int []seq=new int[vertex+1];
-		queue.add(v);
-		visited[start]=true;
-		seq[v]=++cnt;
+	static void bfs(int node) {
+		Queue<Integer> queue=new LinkedList<>();
+		queue.add(node);
+		visited[node]=cnt;
 		
 		while(!queue.isEmpty()) {
-			int now=queue.poll();
+			int xNode=queue.poll();
 			
-			for(int i=0; i<graph.get(now).size(); i++) {
-				int newVertex= graph.get(now).get(i);
-				if(visited[newVertex]==false) {
-					queue.add(newVertex);
-					visited[newVertex]=true;
-					seq[newVertex]=++cnt;
+			for(int i=0; i<arr.get(xNode).size(); i++) {
+				int newNode=arr.get(xNode).get(i);
+				if(visited[newNode]==0) {
+					queue.add(newNode);
+					++cnt;
+					visited[newNode]=cnt;
 				}
 			}
 		}
-		for(int i=1; i<=vertex; i++) {
-			System.out.println(seq[i]);
-		}
+		
 	}
 
 }
