c9259ba7a04a49188cfe5598d8b3c6cf61e0578e
Minseo Han <readex11@naver.com>
2023-12-30 19:59:33 +0900

    약수 구하기

---
+++ src/Math/n2501.java
@@ -0,0 +1,30 @@
+package Math;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class n2501 {
+
+	public static void main(String[] args)throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		int n=Integer.parseInt(st.nextToken());
+		int k=Integer.parseInt(st.nextToken());
+		
+		for(int i=1; i<=n; i++) {
+			if(n%i==0) {
+				k--;
+			}
+			
+			if(k==0) {
+				System.out.println(i);
+				return;
+			}
+		}
+		
+		System.out.println(0);
+	}
+
+}
